b26baa18d72417136c390923cd427120558c271b
muhsin <115468759+muhsinwtf@users.noreply.github.com>
2024-11-15 13:56:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/next'
+import './globals.css'
 
 export default function RootLayout({
   children,
