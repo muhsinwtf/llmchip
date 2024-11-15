117b722bb6859431473ca8e465a4da5e18c86e8f
muhsin <115468759+muhsinwtf@users.noreply.github.com>
2024-11-15 13:52:00 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
+    </html>
+  )
+} 
